d2f35fa1f43e858de29235467c996353df012eb1
Timothy Barrett <tim.barrett.jr@gmail.com>
2016-11-03 19:57:46 -0700

    rough draft

--- Digital Clock/ViewController.swift
+++ Digital Clock/ViewController.swift
@@ -10,9 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var clockColorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var backgroundSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var settingsView: UIView!
+    @IBOutlet weak var settingsButton: UIButton!
+    
+    var timer = Timer()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(ViewController.updateTimer), userInfo: nil, repeats: true)
+        
+        chooseClockColor()
+        chooseBackgroundColor()
+        settingsView.isHidden = true
+        settingsButton.alpha = 0.25
+        timeLabel.alpha = 0
+        settingsButton.layer.cornerRadius = 10
+        settingsView.layer.cornerRadius = 10
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +39,78 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func clockColorSegmentControlChosen(_ sender: Any) {
+        
+        chooseClockColor()
+    }
 
+    
+    @IBAction func backgroundColorSegmentedControlChosen(_ sender: Any) {
+        
+        chooseBackgroundColor()
+    }
+    
+    @IBAction func settingsButtonTapped(_ sender: Any) {
+        if settingsView.isHidden == true {
+            
+            settingsView.isHidden = false
+            settingsButton.setTitle("Hide Settings", for: .normal)
+            settingsButton.alpha = 1.0
+            
+        } else {
+            
+            settingsView.isHidden = true
+            settingsButton.setTitle("Show Settings", for: .normal)
+            settingsButton.alpha = 0.25
+        }
+    }
+    
+    
+    
+    func chooseClockColor() {
+        
+        switch clockColorSegmentedControl.selectedSegmentIndex {
+        case 0:
+            timeLabel.textColor = UIColor.white
+        case 1:
+            timeLabel.textColor = UIColor.black
+        case 2:
+            timeLabel.textColor = UIColor.red
+        case 3:
+            timeLabel.textColor = UIColor.green
+        default:
+            break
+        }
+        
+    }
+    
+    func chooseBackgroundColor() {
+        
+        switch backgroundSegmentedControl.selectedSegmentIndex {
+        case 0:
+            timeLabel.backgroundColor = UIColor.black
+        case 1:
+            timeLabel.backgroundColor = UIColor.white
+        case 2:
+            timeLabel.backgroundColor = UIColor.yellow
+        case 3:
+            timeLabel.backgroundColor = UIColor.blue
+        default:
+            break
+        }
+    }
+    
+    func updateTimer() {
+        
+        let timeformatter = DateFormatter()
+        
+        timeformatter.timeStyle = .medium
+        
+        timeLabel.text = timeformatter.string(from: Date())
+        
+        timeLabel.alpha = 1.0
+        
+    }
+    
 }
 
